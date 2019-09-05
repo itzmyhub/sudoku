b87ab98aa7c058e9ecae4f3cb4c0dd676a63a0a6
Italo <italo.machado@uft.edu.br>
2019-09-05 00:36:50 -0300

    sudoku dont anymore

--- createsudoku.c
+++ createsudoku.c
@@ -1,77 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
-//void alocation(int **matriz, int *vetor);
+void alocation(int ***matriz, int **vetor);
 void createdinamic(int ***matriz, int **vetor);
 void show(int **matriz);
-void showVector(int *vector);
+//void tryagain();
 
 int main(){
-    int **sudoku, *possibility, option, i, j;
+    int **sudoku, *possibility, option;
     createdinamic(&sudoku, &possibility);
     while(1){
     scanf("%d", &option);
         switch(option){
         case 1:
-        //alocation(sudoku, possibility);
+        alocation(&sudoku, &possibility);
         break;       
         case 2:
         show(sudoku);
-        showVector(possibility);
         break;
         }
     }
     free(sudoku);
+    free(possibility);
     return 0;
 }
 
-
-/*void alocation(int **matriz, int *vetor){
-    int row=9, random, i, number=1;
-    while(number <=9){
-        for (i = 0; i < row; i++){
-        random = rand()%9;
-        vetor[random]=1;
-        matriz[i][random] = number;
-    	   //}	else {
-    		//i--;
-    	   //}
-        }
-    number++;
-    }
-}*/
-
 void createdinamic(int ***matriz, int **vetor){
-    int j, row=9, column=9;
-
-   *matriz = (int **)malloc(sizeof(int *) * row);
-   for(int i = 0; i < row; i++) {
+    int row=9;
+    *matriz = (int **)malloc(sizeof(int *) * row);
+    for(int i = 0; i < row; i++) {
         (*matriz)[i] = (int *)malloc(sizeof(int) * row);
         for(int j = 0; j < row; j++) {
             (*matriz)[i][j] = 0;
         }
    }
-    *vetor = (int*)calloc(column, sizeof(int));
+    *vetor = (int*)calloc(row, sizeof(int));
 }
 
-void showVector(int *vector) {
-    int i;
-    for (int i = 0; i < 9; i++)
-    {
-        printf("%d\n", vector[i]);
+void alocation(int ***matriz, int **vetor){
+    int number=1, random, i, sub[3]={0,0,0};
+    srand(time(NULL));
+    while(number <=9){
+        random = rand()%9;
+        printf("%d\n", random);
+        puts("aaaaaaaa");
+        /*if(random < 3)
+        sub[i] = 1;
+        else if (random < 6)
+        sub[i] = 2;
+        else if (random < 9)
+        sub[i] = 3;*/
+        printf("%d %d\n", *vetor[0], *matriz[i][random]); 
+        if (*vetor[random] == 0 && (*matriz)[i][random] == 0){
+            (*matriz)[i][random] = number;
+            *vetor[random] = 1;
+            printf("aaaaaaaaaaaaaaa");
+            } else {
+                i--;
+            }    
+    sub[0] = 0; sub[1] = 0; sub[2] = 0;
+    number++;
     }
 }
 
 void show(int **matriz){
-    int row, spine, i, j;
-    /*for (i = 0; i < 9; i++)
-    {
-        for (j = 0; j < 9; j++)
-        {
-            printf("%d", matriz[i][j]);
-        }
-        printf("\n");
-    }*/ 
+    int row, spine;
+
     puts("O jogo atual é:");
     puts("┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
         for (row = 0; row < 9; row++) {
